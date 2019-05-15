c2842bb3a32c25138f8d01508de4bd24e7f3022c
aaron feldman <aaronalex.feldman@gmail.com>
2019-05-15 16:51:59 -0400

    IFFFFFF SAVE

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+restaurants = Restaurant.create([{
+              name: 'shyteburger',
+              address: '1800 wrongway',
+              phone_number: '174346',
+              category: 'belgian'
+          }, {
+              name: 'veganize',
+              address: '912 vegan',
+              phone_number: '17456',
+              category: 'chinese'
+          }, {
+              name: 'ten ten',
+              address: '10 ten',
+              category: 'chinese'
+          }, {
+              name: 'eatfood resto',
+              address: '890 todaleft',
+              phone_number: '666666',
+              category: 'japanese'
+          }, {
+              name: 'ey im going for coffee',
+              address: '8543 joeshouse',
+              phone_number: '395273',
+              category: 'italian'
+          }])
